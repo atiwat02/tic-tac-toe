1d8e5ce1ee8b00212e1e4baab3180e1f888f5584
atiwat02 <atiwat.ka.61@ubu.ac.th>
2020-09-02 16:54:55 +0700

    Updates

---
+++ precache-manifest.c952f7e5b8e2ce6638494c0f77b8528a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40704f5405b35314e6e930e4df8bfd70",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b1122c4e47013cbe0e0b",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1122c4e47013cbe0e0b",
+    "url": "/tic-tac-toe/static/js/main.d05544b2.chunk.js"
+  },
+  {
+    "revision": "4ad95f08d6b4d56d4069",
+    "url": "/tic-tac-toe/static/js/runtime-main.fa7aabe4.js"
+  }
+]);
